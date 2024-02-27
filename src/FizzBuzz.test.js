02e71c54440bd36aa77bf45b5f18a9d7effcddaf
Diegodenac <diegocanedo917@gmail.com>
2024-02-27 04:09:22 -0400

    Correccion prueba y funcionalidad para generar una secuencia con regla FizzBuzz generar(1) => 1)

--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -1,28 +1,36 @@
+import generarSecuencia from "./FizzBuzzer.js";
 import generarFizzBuzz from "./FizzBuzzer.js";
 
 describe("Generar Fizz-Buzz-FizzBuz", () => {
-  it("deberia generar una secuencia hasta el mismo numero para un numero sin regla [1]", () => {
+  it("deberia generar el mismo numero para un numero sin regla [1]", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
   });
-  it("deberia generar una secuencia hasta el mismo numero para un numero sin regla [2]", () => {
-    expect(generarFizzBuzz(2)).toEqual("1, 2");
+  it("deberia generar el mismo numero para un numero sin regla [2]", () => {
+    expect(generarFizzBuzz(2)).toEqual("2");
   });
-  it("deberia generar una secuencia hasta Fizz para un numero multiplo de 3 [3]", () => {
-    expect(generarFizzBuzz(3)).toEqual("1, 2, Fizz");
+  it("deberia generar Fizz para un numero multiplo de 3 [3]", () => {
+    expect(generarFizzBuzz(3)).toEqual("Fizz");
   });
-  it("deberia generar una secuencia hasta Buzz para un numero multiplo de 5 [5]", () => {
-    expect(generarFizzBuzz(5)).toEqual("1, 2, Fizz, 4, Buzz");
+  it("deberia generar Fizz para un numero multiplo de 3 [6]", () => {
+    expect(generarFizzBuzz(6)).toEqual("Fizz");
   });
-  it("deberia generar una secuencia hasta Fizz para un numero multiplo de 3 [6]", () => {
-    expect(generarFizzBuzz(6)).toEqual("1, 2, Fizz, 4, Buzz, Fizz");
+  it("deberia generar Buzz para un numero multiplo de 5 [5]", () => {
+    expect(generarFizzBuzz(5)).toEqual("Buzz");
   });
-  it("deberia generar una secuencia hasta Buzz para un numero multiplo de 5 [10]", () => {
-    expect(generarFizzBuzz(10)).toEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz");
+  it("deberia generar Buzz para un numero multiplo de 5 [10]", () => {
+    expect(generarFizzBuzz(10)).toEqual("Buzz");
   });
-  it("deberia generar una secuencia hasta FizzBuzz para un numero multiplo de 3 y 5 [15]", () => {
-    expect(generarFizzBuzz(15)).toEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz");
+  it("deberia generar FizzBuzz para un numero multiplo de 3 y 5 [15]", () => {
+    expect(generarFizzBuzz(15)).toEqual("FizzBuzz");
   });
   it("deberia generar FizzBuzz para un numero multiplo de 3 y 5 [30]", () => {
-    expect(generarFizzBuzz(30)).toEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz");
+    expect(generarFizzBuzz(30)).toEqual("FizzBuzz");
   });
 });
+
+describe("Generar secuencia Fizz-Buzz-FizzBuz", () => {
+  it("deberia generar una secuencia hasta el mismo numero para un numero sin regla [1]", () => {
+    expect(generarSecuencia(1)).toEqual("1");
+  });
+});
+
--- src/FizzBuzzer.js
+++ src/FizzBuzzer.js
@@ -1,23 +1,17 @@
 function generarFizzBuzz(n) {
-  let message = "";
+  if(n%3 === 0 && n%5 === 0)
+    return "FizzBuzz";
+  if(n%3 === 0)
+    return "Fizz";
+  if(n%5 === 0)
+    return "Buzz";
+  return n.toString();
+}
+
+function generarSecuencia(n) {
   let i=1;
-  while (i<=n){
-    if(i%3 === 0 && i%5 === 0)
-      message = message + "FizzBuzz";
-    else{
-      if(i%3 === 0)
-        message = message + "Fizz";
-      else{
-        if(i%5 === 0)
-          message = message + "Buzz";
-        else
-          message = message + i.toString();
-      }
-    }
-    if(i<n)
-      message = message + ", ";
-    i++;
-  }
+  let message = "";
+  message = message + generarFizzBuzz(1);
   return message;
 }
 
